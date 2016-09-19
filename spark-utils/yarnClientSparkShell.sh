072a58441ce0d29cbcaf922b3116448d47b454ff
zhuwbigdata <zhuw.bigdata@gmail.com>
2016-09-19 10:13:28 -0700

    Added spark utils

---
+++ spark-utils/yarnClientSparkShell.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/../bin/setenv.sh
+#$SPARK_HOME/bin/spark-shell --master yarn-client --driver-memory 512m --executor-memory 2024m
+$SPARK_HOME/bin/spark-shell --master yarn-client 
