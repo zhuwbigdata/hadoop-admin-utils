1a9909cbebf48f6c5a68454fa7b1f4e1f9cd8974
zhuw.bigdata <zhuw.bigdata@gmail.com>
2016-10-16 19:26:44 -0700

    More on YARN api

---
+++ yarn-utils/am-mr-api/getMRTaskAttemptsByAppIdAndJobIdAndTasksId.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+check_usage() {
+  if [ $# -lt 1 ]
+  then
+    echo "Usage:"
+    echo "$0 <app_id> <job_id> <task_id>"
+    exit 1
+  fi
+}
+
+check_usage $*
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/setenv.sh
+URL=http://${YARN_PROXY_SERVER}:${YARN_PROXY_PORT}/proxy/${1}/ws/v1/mapreduce/jobs/${2}/tasks/${3}/attempts
+#echo curl -X GET  $URL 
+curl -L -X GET  $URL | python -mjson.tool
