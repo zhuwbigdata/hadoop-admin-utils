0818837a170e6073e45619bdd9b030fb64f73ee1
Wayne Zhu <zhuw.bigdata.com>
2016-10-17 13:00:11 -0700

    More on spark

--- spark-utils/yarnClientSparkShell.sh
+++ spark-utils/yarnClientSparkShell.sh
@@ -2,4 +2,4 @@
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/../bin/setenv.sh
 #$SPARK_HOME/bin/spark-shell --master yarn-client --driver-memory 512m --executor-memory 2024m
-$SPARK_HOME/bin/spark-shell --master yarn-client 
+$SPARK_HOME/bin/spark-shell --master yarn --deploy-mode client 
