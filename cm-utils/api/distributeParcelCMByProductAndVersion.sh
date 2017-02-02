be82c106d297de148cf7486d46b88cd421834a69
Cloud User <ec2-user@ip-10-3-1-88.ec2.internal>
2017-02-01 20:42:29 -0500

    More on CM Parcel

---
+++ cm-utils/api/distributeParcelCMByProductAndVersion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+check_usage() {
+  if [ $# -lt 2 ];
+  then
+    echo "Usage:"
+    echo "$0 <PRODUCT,e.g. SPARK> <VERSION>"
+    exit 1
+  fi
+}
+check_usage $*
+
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/setenv.sh
+curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/clusters/${CM_CLUSTER}/parcels/products/${1}/versions/${2}/commands/startDistribution
