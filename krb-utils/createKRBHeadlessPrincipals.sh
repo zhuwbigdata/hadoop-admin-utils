db24f659c2cd5a95c87383537da20c055e1816d9
Wayne Zhu <Wayne.Zhu.consultant@nielsen.com>
2017-02-04 21:09:43 -0600

    More on KRB

---
+++ krb-utils/createKRBHeadlessPrincipals.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+check_usage() {
+  if [ $# -lt 1 ]
+  then
+    echo "Usage:"
+    echo "$0  [krb_principals_file, one principal per line]"
+    exit 1
+  fi
+}
+check_usage $*
+KRB_PRINCS_FILE=$1
+KRB_ADMIN_USER=$(cat .krb_admin_user)
+KRB_ADMIN_PASS=$(cat .krb_admin_pass)
+while IFS= read -r line
+do
+    kadmin.local  -p ${KRB_ADMIN_USER} -w ${KRB_ADMIN_PASS}  -q "addprinc -randkey ${line}"
+    kadmin.local  -p ${KRB_ADMIN_USER} -w ${KRB_ADMIN_PASS}  -q "xst -k  ${line}.keytab ${line}"
+done < ${KRB_PRINCS_FILE}
