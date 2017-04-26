d7701cfd6e14113f3c20aac63a56c4ee145fe8d7
Wayne Zhu <zhuw.bigdata@gmail.com>
2017-04-26 14:58:21 -0400

    More on CM

---
+++ cm-utils/api/getCMConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/setenv.sh
+curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/cm/service/config?view=full
+#curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/cm/service/config?view=summary
--- cm-utils/api/getCMServices.sh
+++ cm-utils/api/getCMServices.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/setenv.sh
-curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET  http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/cm/service?view=summary
+#curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET  http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/cm/service?view=summary
+curl -u "${CM_ADMIN}:${CM_PASS}" -i -X GET  http://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/cm/service?view=full
