3fe92eb4edc7c1e911618eaaad490e990ced40f5
Wayne Zhu <zhuw.bigdata@gmail.com>
2017-05-04 12:12:26 -0400

    More on impala

---
+++ beeline-utils/mrHQL.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+check_usage() {
+  if [ $# -lt 1 ]
+  then
+    echo "Usage:"
+    echo "$0  <hql_file_path>"
+    exit 1
+  fi
+}
+check_usage $*
+
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/../bin/setenv.sh
+if [ "$KRB_ENABLED" = true ]; then
+  beeline -u $BEELINE_JDBC_URL --verbose=true --hiveconf hive.execution.engine=mr --hiveconf USER=$(whoami) --force=false --showWarnings=true --showNestedErrs=true -f $1 
+else
+  beeline -n $(whoami) -p $(cat mypass) -u $BEELINE_JDBC_URL --verbose=true --hiveconf hive.execution.engine=mr --hiveconf USER=$(whoami) --force=false --showWarnings=true --showNestedErrs=true -f $1 
+fi
--- bin/setenv.sh
+++ bin/setenv.sh
@@ -4,7 +4,6 @@ NN_MASTERS=$(uname -n)
 MASTERS=$(uname -n)
 SLAVES=$(uname -n)
 KAFKA_BROKERS=$(uname -n):9092
-IMPALA_URI=$(uname -n):21000
 OOZIE_URI=http://$(uname -n):11000/oozie 
 SQOOP_JDBC_URL="jdbc:oracle:thin:@//$(uname -n):1521/xe"
 YARN_URI=$(uname -n):8088
@@ -15,7 +14,7 @@ export NN_MASTERS=$(eval echo $NN_MASTERS);
 export ZK_QUORUM=prodnj-lnx-hdpwkr01.mio.local:2181,prodnj-lnx-hdpwkr02.mio.local:2181,prodnj-lnx-hdpwkr03.mio.local:2181
 export SPARK_HOME=/opt/cloudera/parcels/CDH-5.11.0-1.cdh5.11.0.p0.34/lib/spark
 export KAFKA_BROKERS
-export IMPALA_URI
+export IMPALA_URI=prodnj-lnx-hdpedge01.mio.local:21050
 export OOZIE_URI
 export SQOOP_JDBC_URL
 export KRB_ENABLED=true
