6c2cabc52733377b6b6fc8616ca41c46f90ad8d0
Wayne Zhu <wayne_zhu@prodva-hdpedge01.miop.aws>
2018-02-15 14:31:26 -0500

    More on beeline

--- beeline-utils/mrBL.sh
+++ beeline-utils/mrBL.sh
@@ -2,7 +2,7 @@
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/../bin/setenv.sh
 if [ "$KRB_ENABLED" = true ]; then
-  beeline -u $BEELINE_JDBC_URL --color=true --hiveconf hive.execution.engine=mr --hiveconf mapreduce.queue.name=default 
+ echo beeline -u $BEELINE_JDBC_URL --color=true --hiveconf hive.execution.engine=mr --hiveconf mapreduce.queue.name=default 
 else
   beeline -n $(whddoami) -p $(cat mypass) -u $BEELINE_JDBC_URL --color=true --hiveconf hive.execution.engine=mr --hiveconf mapreduce.queue.name=default 
 fi
