45bf4e7137a85fe68b82d6692beb4a9929362aa5
Wayne Zhu <Wayne.Zhu.consultant@nielsen.com>
2018-02-19 10:28:02 -0600

    More on api

--- cm-utils/python/getConfig2.py
+++ cm-utils/python/getConfig2.py
@@ -29,13 +29,39 @@ CONFIG_KEY_VALUE_MAP = {
   'HBASE_REST_PORT': None,                                                                   
   'KAFKA_BROKER': None,                
 }
-    
 
-def getKeyValueByServiceTypeAndRoleType(cluster, service_type, role_type, key_in):
-  value_out = None
-  for s in cluster.get_all_services():
+HOST_NAME2ID_MAP = {}
+HOST_ID2NAME_MAP = {} 
+
+def getServiceByServiceType(clusterRef, service_type):
+  service_out = None
+  for s in clusterRef.get_all_services():
     if s.type == service_type:
-      print 'SERVICE:', s.type, s.get_config()
+      service_out = s
+  return service_out
+
+def getRCGByServiceAndRoleType(serviceRef, role_type):
+  rcg_out = None
+  service_role_group_list  = serviceRef.get_all_role_config_groups()
+  for x in service_role_group_list:
+    if x.roleType == role_type:
+      rcg_out = x
+  print rcg_out
+  return rcg_out
+
+def geValueByKeyInRCG(rcgRef, key_in):
+  value_out = None
+  for key, val  in rcgRef.get_config().items():
+    #print  'Key:', key, 'Value:', val
+    if key == key_in:
+        value_out = val
+  return value_out
+
+def getKeyValueByServiceTypeAndRoleType(clusterRef, service_type, role_type, key_in):
+  value_out = None
+  for s in clusterRef.get_all_services():
+    if s.type == service_type:
+      #print 'SERVICE:', s.type, s.get_config()
       service_role_group_list  = s.get_all_role_config_groups()
       for x in service_role_group_list:
         if x.roleType == role_type:
@@ -46,6 +72,16 @@ def getKeyValueByServiceTypeAndRoleType(cluster, service_type, role_type, key_in
                 value_out = val
   return value_out
 
+def getHostsByServiceAndRoleType(serviceRef, role_type):
+  hosts_out = []
+  service_role_list =  serviceRef.get_all_roles()
+    for x in service_role_list:
+      #print 'ROLE:', x.type, x.get_config()
+      if x.type == role_type:
+        hosts_out.append(HOST_ID2NAME_MAP[x.hostRef.hostId])
+        #print x.to_json_dict()
+  return hosts_out
+
 
 def main(cm_fqhn, cm_user_name, cm_user_password, cm_cluster_name): 
   #print cm_fqhn, cm_user_name, cm_user_password, cm_cluster_name
@@ -53,29 +89,34 @@ def main(cm_fqhn, cm_user_name, cm_user_password, cm_cluster_name):
   
   # Get a list of all clusters
   cdh_cluster = None
-  host_id2name_map = {}
-  host_name2id_map = {}
+  HOST_ID2NAME_MAP = {}
+  HOST_NAME2ID_MAP = {}
   
   for c in api.get_all_clusters():
     if c.name == cm_cluster_name:
       print '\nCluster:', c
       cdh_cluster = c
       for x in cdh_cluster.list_hosts():
-        host_name2id_map[api.get_host(x.hostId).hostname] =  x.hostId
-        host_id2name_map[x.hostId] = api.get_host(x.hostId).hostname 
+        HOST_NAME2ID_MAP[api.get_host(x.hostId).hostname] =  x.hostId
+        HOST_ID2NAME_MAP[x.hostId] = api.get_host(x.hostId).hostname 
       print '\nHostName to HostId Mapping:'
-      for x in host_name2id_map:
-        print  x, host_name2id_map[x]
+      for x in HOST_NAME2ID_MAP:
+        print  x, HOST_NAME2ID_MAP[x]
       print '\nHostId to HostName Mapping:'
-      for x in host_id2name_map:
-        print  x, host_id2name_map[x]
+      for x in HOST_ID2NAME_MAP:
+        print  x, HOST_ID2NAME_MAP[x]
       print '\nServices:'
       for x in cdh_cluster.get_all_services():
         print x.type
-      zk_client_port = getKeyValueByServiceTypeAndRoleType(cdh_cluster, 
-                                          SERVICE_TYPE_MAP['zookeeper'], 
-                                          SERVICE_ROLE_TYPE_MAP['zookeeper'],
-                                          'clientPort');
+      
+      #ZooKeeper
+      #zk_client_port = getKeyValueByServiceTypeAndRoleType(cdh_cluster, 
+      #                                    SERVICE_TYPE_MAP['zookeeper'], 
+      #                                    SERVICE_ROLE_TYPE_MAP['zookeeper'],
+      #                                    'clientPort');
+      zk_service  = getServiceByServiceType(cdh_cluster, SERVICE_TYPE_MAP['zookeeper'])
+      zk_rcg      = getRCGByServiceAndRoleType(zk_service, SERVICE_ROLE_TYPE_MAP['zookeeper'])
+      zk_client_port = geValueByKeyInRCG(zk_rcg, 'clientPort')
       if zk_client_port != None:
         CONFIG_KEY_VALUE_MAP['ZOOKEEPER_PORT'] = zk_client_port
       print CONFIG_KEY_VALUE_MAP
