dcab9a7b59fe02cdbea88a80462f2839941e400a
wayne_zhu <prdservices@quickstart.cloudera>
2018-02-18 16:45:37 -0800

    More on api

--- cm-utils/api/listRCGByServiceNameAndRoleConfigGroupName.sh
+++ cm-utils/api/listRCGByServiceNameAndRoleConfigGroupName.sh
@@ -11,4 +11,4 @@ check_usage $*
 
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/setenv.sh
-echo ${CURL_CMD} -u "${CM_ADMIN}:${CM_PASS}" -i -X GET ${HTTP_PROTOCOL}://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/clusters/${CM_CLUSTER}/services/$1/roleConfigGroups/$2?view=full
+${CURL_CMD} -u "${CM_ADMIN}:${CM_PASS}" -i -X GET ${HTTP_PROTOCOL}://${CM_SERVER}:${CM_PORT}/api/${CM_VERSION}/clusters/${CM_CLUSTER}/services/$1/roleConfigGroups/$2?view=full
