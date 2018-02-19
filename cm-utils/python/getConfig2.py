e302c776e189f806eecbf4cc38fd5aab8e493345
Wayne Zhu <Wayne.Zhu.consultant@nielsen.com>
2018-02-19 12:46:37 -0600

    More on api

--- cm-utils/python/getConfig2.py
+++ cm-utils/python/getConfig2.py
@@ -166,7 +166,7 @@ def main(cm_fqhn, cm_user_name, cm_user_password, cm_cluster_name):
       #YARN
       yarn_service  = getServiceByServiceType(cdh_cluster, SERVICE_TYPE_MAP['yarn'])
       #inspectRCGs(yarn_service)
-      yarn_jt_rcg      = getRCGByServiceAndRoleType(yarn_service, SERVICE_ROLE_TYPE_MAP[''resourcemanager''])
+      yarn_jt_rcg      = getRCGByServiceAndRoleType(yarn_service, SERVICE_ROLE_TYPE_MAP['resourcemanager'])
       inspectKVsInRCG(yarn_jt_rcg)
         
       
