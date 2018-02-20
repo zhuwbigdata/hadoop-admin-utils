11ee580aa12114665ddb075a479d569a60ef2f2f
raj_ops <raj_ops@sandbox-hdp.hortonworks.com>
2018-02-20 20:23:27 +0000

    More on ambari

--- ambari-utils/api/setenv.sh
+++ ambari-utils/api/setenv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-uname=$(uname -n)
 AMBARI_SERVER=localhost
-AMBARI_CLUSER=sandbox
-export AMBARI_SERVER;
-export AMBARI_CLUSER;
-export AMBARI_PORT=8443;
+AMBARI_CLUSER=Sandbox
+export AMBARI_SERVER
+export AMBARI_CLUSER
+export AMBARI_PORT=8080
 export AMBARI_SCRIPTS_DIR=/var/lib/ambari-server/resources/scripts
 export AMBARI_ADMIN=admin
 export AMBARI_PASS=$(cat admin.pass)
