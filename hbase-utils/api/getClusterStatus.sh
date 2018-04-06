4c6ed5bad1867e3e590c980f5665da0da2c76a05
wayne zhu <zhuw.bigdata@gmail.com>
2018-04-06 17:48:33 -0400

    More on hbase

--- hbase-utils/api/getClusterStatus.sh
+++ hbase-utils/api/getClusterStatus.sh
@@ -1,13 +1,4 @@
-#!/bte -u : 'in/bash
-check_usage() {
-  if [ $# -lt 1 ];
-  then
-    echo "Usage:"
-    echo "$0 <SERVICE_NAME>"
-    exit 1
-  fi
-}
-check_usage $*
-/usr/bin/curl --insecure --negotiate -u :  ${HTTP_PROTOCOL}://${HBASE_RESTSVR_HOST}:${HBASE_RESTSVR_PORT}/status/cluster
-export HBASE_RESTSVR_HOST=
-
+#!/bin/bash
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/setenv.sh
+${CURL_CMD}  "${HTTP_PROTOCOL}://${HBASE_RESTSVR_HOST}:${HBASE_RESTSVR_PORT}/status/cluster"
