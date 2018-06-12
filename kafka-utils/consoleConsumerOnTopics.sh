57aa4e039d80cede052fb1c9de1560889ce31782
Wayne Zhu <Wayne.Zhu.consultant@nielsen.com>
2018-06-12 17:35:24 -0400

    More on kafka

--- kafka-utils/consoleConsumerOnTopics.sh
+++ kafka-utils/consoleConsumerOnTopics.sh
@@ -10,5 +10,6 @@ check_usage() {
 check_usage $*
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/../bin/setenv.sh
+export KAFKA_OPTS="-Djava.security.auth.login.config=./jaas.conf"
 #/usr/bin/kafka-console-consumer --zookeeper $ZK_QUORUM --topic $1 --from-beginning
 /usr/bin/kafka-console-consumer --new-consumer --bootstrap-server $KAFKA_BS_SERVER --topic $1 --from-beginning --consumer.config consumer.properties
--- kafka-utils/consoleProducerOnTopics.sh
+++ kafka-utils/consoleProducerOnTopics.sh
@@ -10,5 +10,6 @@ check_usage() {
 check_usage $*
 BASE_DIR=$(dirname $0)
 source $BASE_DIR/../bin/setenv.sh
+export KAFKA_OPTS="-Djava.security.auth.login.config=./jaas.conf"
 #/usr/bin/kafka-console-producer  --broker-list $KAFKA_BROKERS --topic $1
 /usr/bin/kafka-console-producer  --broker-list $KAFKA_BROKERS --topic $1 --producer.config producer.properties
